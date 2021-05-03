a1360ee45206907b745cca32a9ba30246cf488c2
Sahil Sharma <sahil98sharma@gmail.com>
2021-05-03 20:36:52 +0530

    uploading existing project to Git

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if(bird != null)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if(collision.contacts[0].normal.y < -0.5 || 
+           collision.contacts[0].normal.x < -0.5 )
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+
+
+    }
+}
